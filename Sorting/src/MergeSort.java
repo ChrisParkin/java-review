9e1ece915f87f125367aae37f9a3207d8b62ad8f
Chris Parkin <chris.d.parkin@gmail.com>
2015-04-21 11:24:09 -0400

    MergeSort

---
+++ Sorting/src/MergeSort.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MergeSort {
+
+	public static void mergesort(char[] s, int start, int stop){
+		int middle;
+		
+		if(start < stop){
+			middle = (int)(start + stop) / 2;
+			mergesort(s, start, middle);
+			mergesort(s, middle+1, stop);
+			merge(s, start, middle, stop);
+		}
+	}
+	
+	public static void merge(char[] s, int start, int middle, int stop){
+		
+		int i; // counter
+		
+		LinkedList<Character> buff1 = new LinkedList<>();
+		LinkedList<Character> buff2 = new LinkedList<>();
+		
+		// get buffers ready
+		for(i = start; i <= middle; i++){
+			buff1.add(s[i]);
+		}
+		for(i = middle+1; i <= stop; i++){
+			buff2.add(s[i]);
+		}
+		
+		i = start;
+		while(!(buff1.isEmpty() || buff2.isEmpty())){
+			if(buff1.peek() <= buff2.peek()){
+				s[i++] = buff1.pop();
+			}else{
+				s[i++] = buff2.pop();
+			}
+		}
+		
+		while(!buff1.isEmpty()) s[i++] = buff1.pop();
+		while(!buff2.isEmpty()) s[i++] = buff2.pop();
+	}
+	
+	public static void main(String[] args){
+		char[] myname = "HEREISMYAMAZINGSTRING".toCharArray();
+		mergesort(myname,0, myname.length - 1);
+		System.out.println(Arrays.toString(myname));
+	}
+}
